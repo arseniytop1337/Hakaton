402fccefb17e3cfbbbc03afb55f925163bf222db
Тимур Садртдинов <timursadrtdinov@MacBook-Air-Timur-3.local>
2026-05-31 19:41:53 +0300

    Добавил reporter.py для вывода статистики

--- app/reporter.py
+++ app/reporter.py
@@ -1,4 +1,17 @@
 #Формирует отчет о работе системы.
 #Сколько писем обработано, сколько попало в каждую категорию, сколько файлов не удалось прочитать,
 #какие письма ушли в unknwown
+from app.stats import get_stats
 
+def generate_report():
+    stats = get_stats()
+
+    print("\n--- ИТОГОВЫЙ ОТЧЕТ О РАБОТЕ ---")
+    print(f"Всего обработано файлов: {stats['total_processed']}")
+    print(f"Ошибки при перемещении: {stats['errors']}")
+
+    print("\nРазбивка по категориям:")
+    for category, count in stats['categories'].items():
+        print(f"- {category}: {count}")
+
+    print("-------------------------------\n")
